0795097d416df443d620d082f59837064ddc958a
Jackie Cho <jackie.s.cho@gmail.com>
2022-04-28 11:54:43 -0400

    Add web app files

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route("/")
 def index():
